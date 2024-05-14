8cc967e956fa01999714a6aa8ccb9d3de603ba8e
Tamás Fodor <fodor.tamas48@gmail.com>
2024-05-14 17:54:17 +0200

    Added a package to log the HTTP requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 import { createTournamentsRouter } from "./tournaments/router.js";
 
 export function createApp(dependencies) {
     const app = express();
+    app.use(morgan('combined'));
     app.use(express.json());
 
     app.use("/tournaments", (req, res, next) => {
