8b8dc688c86db0ca40c1683a56d00c5fc4026cd0
Tamás Fodor <fodor.tamas48@gmail.com>
2024-05-14 11:27:19 +0200

    Created an endpoint to delete a record from the db

--- src/tournaments/router.js
+++ src/tournaments/router.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-export function createTournamentsRouter({ loadTournaments, saveTournament }) {
+export function createTournamentsRouter({ loadTournaments, saveTournament, deleteTournament }) {
     const tournamentsRouter = express.Router();
 
     tournamentsRouter.get('', (req, res, next) => {
@@ -13,9 +13,21 @@ export function createTournamentsRouter({ loadTournaments, saveTournament }) {
 
     tournamentsRouter.post('', (req, res, next) => {
         try {
-            console.log(req.body)
             saveTournament(req.body);
             res.sendStatus(201);
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    tournamentsRouter.delete('', (req, res, next) => {
+        try {
+            deleteTournament({
+                event_type: req.query.event_type,
+                category: req.query.category,
+                city: req.query.city
+            });
+            res.sendStatus(204);
         } catch (err) {
             next(err);
         }
--- src/tournaments/sqlite-storage.js
+++ src/tournaments/sqlite-storage.js
@@ -11,6 +11,10 @@ export function connectToDb(filepath) {
         saveTournament(tournament) {
             const statement = db.prepare("INSERT INTO tournaments (event_type, category, city, country, surface) VALUES (?, ?, ?, ?, ?)");
             statement.run(tournament.event_type, tournament.category, tournament.city, tournament.country, tournament.surface);
+        },
+        deleteTournament(tournament) {
+            const statement = db.prepare("DELETE FROM tournaments WHERE event_type = ? AND category = ? AND city = ?");
+            statement.run(tournament.event_type, tournament.category, tournament.city);
         }
     }
 }
